85960cc29bff3f2be294227b266b9176e8280cf5
Charles ugochukwu <Charlkeyz2017@gmail.com>
2024-07-16 22:41:03 +0100

    pagination

--- src/components/ContextAPI/ContextApi.jsx
+++ src/components/ContextAPI/ContextApi.jsx
@@ -11,12 +11,15 @@ export const ContextApiProvider = ({children}) => {
     const [searchInput, setSearchInput] = useState("")
     const [selectAll, setSelectAll] = useState(false)
     const [filteredItem, setFilteredItem] = useState([])
+    const [currentPage, setCurrentPage] = useState(0)
+    const [totalItems, setTotalItems] = useState(0)
+    const itemsPerPage = 10
 
 
 
     const fetchApiData = async () => {
         try {
-            const response = await axios.get('http://3.88.1.181:8000/products/public/catalog?supplier=FragranceX&first=0&last=50');
+            const response = await axios.get('http://3.88.1.181:8000/products/public/catalog?supplier=FragranceX&first=0&last=100');
             setData(response.data)
             setFilteredItem(response.data)
             
@@ -33,19 +36,38 @@ export const ContextApiProvider = ({children}) => {
     },[])
 
     // for the searchInput fields 
-    useEffect(()=> {
+    useEffect(()=> {  
         if(data){
-        setFilteredItem(
-            data.filter((item)=> 
-                item.Name.toLowerCase().includes(searchInput.toLowerCase()) ||
-                item.SKU.toString().toLowerCase().includes(searchInput.toLowerCase()) ||
-                item.Title.toLowerCase().includes(searchInput.toLowerCase()) ||
-                item.Description.toLowerCase().includes(searchInput.toLowerCase()) ||
-                item.Brand.toLowerCase().includes(searchInput.toLowerCase())
-            )
+            const filtered = data.filter((item, index) => {
+                const sn = index + 1
+                return (
+                    sn.toString().includes(searchInput.toLowerCase()) ||
+                    item.Name.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    item.SKU.toString().toLowerCase().includes(searchInput.toLowerCase()) ||
+                    item.Title.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    item.Description.toLowerCase().includes(searchInput.toLowerCase()) ||
+                    item.Brand.toLowerCase().includes(searchInput.toLowerCase())
+                )
+            } 
+            
         )
-    }
+        setFilteredItem(filtered)
+        setTotalItems(filtered.length)
+        setCurrentPage(0)
+        
+            
+        }
+        
+       
+    
     },[searchInput, data])
+
+    // useEffect(()=> {
+    //     setCurrentPage(0)
+    // },[searchInput])
+
+    
+    
     // Table body ckeckbox
     const handleCheckBox = (id) => {
         setSelectedItem((prevSelectedItem) => {
@@ -58,19 +80,52 @@ export const ContextApiProvider = ({children}) => {
     }
     // Table header checkbox
     const handleSelectAll = () => {
-        if(selectAll){
+
+        if (selectAll) {
             setSelectedItem([])
-        }else{
-            setSelectedItem(filteredItem.slice(0, 10).map((item) => item.SKU))
+          } else {
+            setSelectedItem(
+              filteredItem
+                .slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage)
+                .map((item) => item.SKU)
+            
+            );
         }
-        setSelectAll(!selectAll)
+          
+          setSelectAll(!selectAll);
     }
     // search input navbar
     const handleSearchInput = (e) => {
         setSearchInput(e.target.value)
+        
+        // const newFilteredItems = data.filter((item) => 
+        //     item.Name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        //     item.SKU.toString().toLowerCase().includes(e.target.value.toLowerCase()) ||
+        //     item.Title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        //     item.Description.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        //     item.Brand.toLowerCase().includes(e.target.value.toLowerCase())
+        //   )
+        //   setFilteredItem(newFilteredItems)
+          
+        //   // Adjust current page if necessary
+        //   const maxPage = Math.max(0, Math.ceil(newFilteredItems.length / itemsPerPage) - 1)
+        //   if (currentPage > maxPage) {
+        //     setCurrentPage(maxPage)
+        //   }
+    }
+    const indexOfLastItem = (currentPage + 1) * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+
+    const currentItems = filteredItem.slice(indexOfFirstItem, indexOfLastItem);
+    const pageCount = Math.ceil(filteredItem.length / itemsPerPage);
+
+    const handlePageClick = ({selected}) => {
+        setCurrentPage(selected)
+        
     }
 
-    const value = {data, fetchApiData, handleCheckBox, handleSelectAll, handleSearchInput, selectedItem, isLoading, searchInput, filteredItem, setSearchInput, setFilteredItem, selectAll}
+
+    const value = {data, setSelectAll, fetchApiData, handleCheckBox, handleSelectAll, handleSearchInput, selectedItem, isLoading, searchInput, filteredItem, setSearchInput, setFilteredItem, selectAll, itemsPerPage, indexOfFirstItem, setCurrentPage, totalItems, currentPage, currentItems, handlePageClick, pageCount}
 
     return (
         <ContextApi.Provider value={value}>{children}</ContextApi.Provider>
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,24 +2,39 @@
 import  { useContext } from 'react'
 import { ContextApi } from '../ContextAPI/ContextApi'
 import { Headers } from '../ContextAPI/Headers'
+import ReactPaginate from 'react-paginate'
 
 const TableData = () => {
 
-    const { handleCheckBox, handleSelectAll, selectedItem, isLoading, selectAll, filteredItem} = useContext(ContextApi)
+    const { handleCheckBox, itemsPerPage, indexOfFirstItem, totalItems, currentPage, handleSelectAll, selectedItem, isLoading, selectAll, filteredItem, handlePageClick, pageCount, currentItems} = useContext(ContextApi)
+
+  
+    if(isLoading){
+      return <div className='flex justify-center items-center w-screen h-screen'>Loading...</div>
+    }
+
+    // const startinIndex = currentPage * itemsPerPage
+    
+     
+
+    // const pageCount = Math.ceil(filteredItem.length / itemsPerPage)
 
     
-    if(isLoading){
-      return <div className='flex justify-center items-center w-screen h-screen'>...Loading</div>
-    }
+
   return (
     <div className=''>
         <section className='bg-[#F6F6F6] px-10 w-screen h-screen flex flex-col items-start overflow-x-auto'>
             <h1 className='py-10 text-[18px] font-medium'>Department List</h1>
+            <div>
+              <h1>current page {currentPage + 1}</h1>
+              Showing {indexOfFirstItem + 1} - {Math.min(indexOfFirstItem + itemsPerPage, totalItems)} of {totalItems} items
+
+            </div>
            <table className="min-w-full overflow-x-auto font-Table_font text-xs font-medium">
               <thead>
                 <tr className='bg-[#F0F4FE]'>
                   <th>
-                    <input type="checkbox" checked={selectAll} onChange={handleSelectAll} className='accent-blue-600 cursor-pointer'/>
+                    <input type="checkbox" checked={selectAll || (selectedItem.length === filteredItem.length && selectedItem.length > 0)} onChange={handleSelectAll} className='accent-blue-600 cursor-pointer'/>
                   </th>
                   {
                     Headers.map((title, index)=>(
@@ -29,14 +44,16 @@ const TableData = () => {
                 </tr>
               </thead>
               <tr className='h-5'></tr>
-              <tbody className="bg-white overflow-x-auto" >
+              <tbody className="bg-white overflow-x-auto" > 
                 {
-                  filteredItem.slice(0, 10).map((item, index, arr)=> {
+                  currentItems.map((item, index, arr)=> {
                     const {SKU, Image_1, Description, Title, Name, Brand, Quantity, size, ['Cost Price']: costPrice } = item
+                    
+                    const serialNo = filteredItem.indexOf(item) + 1
                     return(
                       <tr key={index} className={`w-full leading-4 border-b-2 sm:text-xs text-[10px] ${index === arr.length - 1 ? 'border-none' : ''}`}>
                         <td className={`py-3 px-5 ${index === 0 ? 'rounded-tl-xl' : ''} ${index === arr.length - 1 ? 'rounded-bl-xl' : ''}`}><input type="checkbox" checked={selectedItem.includes(SKU)} onChange={() => handleCheckBox(SKU)} className='accent-blue-600 cursor-pointer'/></td>
-                        <td className={`py-3 px-5`}>{index + 1}.</td>
+                        <td className={`py-3 px-5`}>{serialNo}.</td>
                         <td className=' sm:w-[40px] sm:h-[40px] py-3 px-5'><img src={Image_1} alt="" /></td>
                         <td className='py-3 px-5'>{SKU}</td>
                         <td className='py-3 px-5'>{Name}</td>
@@ -53,6 +70,27 @@ const TableData = () => {
                 }
               </tbody>
            </table>
+           <div className='flex justify-center items-center w-full overflow-hidden'>
+           <ReactPaginate
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            breakLabel={'...'}
+            onPageChange={handlePageClick}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            containerClassName={'flex justify-center mt-4'}
+            pageClassName={'mx-1'}
+            pageLinkClassName={'block py-2 px-4 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition-colors duration-200'}
+            previousClassName={'mx-1'}
+            previousLinkClassName={'block py-2 px-4 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition-colors duration-200'}
+            nextClassName={'mx-1'}
+            nextLinkClassName={'block py-2 px-4 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition-colors duration-200'}
+            breakClassName={'mx-1'}
+            breakLinkClassName={'block py-2 px-4 border border-gray-300 rounded'}
+            activeClassName={'bg-blue-500 text-white'}
+          />
+           </div>
         </section>
     </div>
 
